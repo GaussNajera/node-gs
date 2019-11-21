ccbb851e36d344ac264cf1f90774af3a8cd0126c
GusNajera <gvt_a9@hotmail.com>
2019-11-20 19:06:55 -0600

    conexion terminada con BD

--- node-gs/app.js
+++ node-gs/app.js
@@ -3,27 +3,12 @@ const app = express();
 const mongoose = require('mongoose');
 const postRoutesd = require("./routes/post");
 const morgan = require('morgan');
-const dotenv = require('dotenv');
-dotenv.config();
+const bodyParser = require('body-parser');
+// dotenv.config();
 
-
-let MongoClient = require('mongodb').MongoClient;
-
-const uri = "mongodb://gauss_najera:onceonceonce112@nodeapi-shard-00-00-s48ar.mongodb.net:27017,nodeapi-shard-00-01-s48ar.mongodb.net:27017,nodeapi-shard-00-02-s48ar.mongodb.net:27017/test?ssl=true&replicaSet=NodeAPI-shard-0&authSource=admin&retryWrites=true&w=majority";
-MongoClient.connect(uri, 
-    {
-    useUnifiedTopology: true,
-    useNewUrlParser: true
-    },
-    function(err, client) {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
-
-//MONGO_URI=mongodb://localhost/nodeapi
+const MONGO_URI = "mongodb+srv://gauss_najera:onceonceonce112@nodeapi-s48ar.mongodb.net/test?retryWrites=true&w=majority";
 mongoose.connect(
-    process.env.MONGO_URI,
+    MONGO_URI,
     {
         useUnifiedTopology: true,
         useNewUrlParser: true
@@ -32,12 +17,9 @@ mongoose.connect(
 .catch(() => console.log("ERRRRRRRRRRRRRRRRRRRRRRRRRRRROR"));
 
 
-
-
 mongoose.connection.on('error', err => {
     console.log(`DB connection error: ${err.message}`);
 })
-
 
 app.use(morgan('dev'));
 let i = true;
@@ -53,8 +35,9 @@ const usingMiddleware = (req, res, next) => {
 };
 
 app.use(usingMiddleware);
-
+app.use(bodyParser.json());
 app.use("/", postRoutesd);
 
-const port = process.env.PORT || 8080;
+// LO QUE ERA ANTES  =====>  const port = process.env.PORT || 8080;
+const port = 8080;
 app.listen(port, () => { console.log(`Running on port: ${port}`) });
--- node-gs/controller/post.js
+++ node-gs/controller/post.js
@@ -11,6 +11,18 @@ exports.getPost = (req, res) => {
 
 exports.createPost = (req, res) => {
     const post = new Post(req.body);
-    console.log("CREATING POST: ", post);
+    console.log("CREATING POST: ", req.body);
+
+    post.save( (err, result)=>{
+        if(err){
+            return res.status(400).json({
+                error: err                
+            }); 
+        }
+
+        res.status(200).json({
+            post: result
+        })
+    });
 
 };
